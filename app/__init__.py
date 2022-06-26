7caf52fc15f20976a4211473682dffe992d6616d
yiazh <65192691+yiazh@users.noreply.github.com>
2022-06-26 09:11:50 +0200

    Chapter 5

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,16 @@ Author: Yi Zheng, Department of Electrical Engineering, DTU
 from flask import Flask
 from config import Config
 
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+
 app = Flask(__name__)
 app.config.from_object(Config)
 
-from app import routes
+db = SQLAlchemy(app)#数据库对象
+migrate = Migrate(app, db)#迁移引擎对象
+login = LoginManager(app)
+login.login_view = 'login'
+
+from app import routes, models
--- microblog.py
+++ microblog.py
@@ -4,7 +4,12 @@ Created on:
 Author: Yi Zheng, Department of Electrical Engineering, DTU
 
 '''
-from app import app
+from app import app, db
+from app.models import User, Post
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
 
 if __name__ == '__main__':
     app.run()
